2adc0831b062a235bcab43b2bdc1952751b7f8bf
Joel Herndon <joel.herndon@gmail.com>
2018-02-01 11:41:52 -0800

    Completed loading a tibble of installed packages and saving them to a csv file for later processing.

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,9 @@
+library(tidyverse)
+library(here)
 ## deja vu from yesterday!
 
 ## create a data frame of your installed packages
+ipt <- installed.packages() %>% as_tibble()
 
 ## keep the variables
 ##   * Package
@@ -8,8 +11,13 @@
 ##   * Version
 ##   * Priority
 ##   * Built
+ipt_selected <- ipt %>% select(Package, LibPath, Version, Priority, Built)
+
+
 
 ## write it to data/installed-packages.csv
+write_csv(ipt_selected,here("data","installed-packages.csv"))
+
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
